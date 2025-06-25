f115efb05fd00e8d42dc85ff089dbb1be0640b88
mengqizh1010 <2283044805@qq.com>
2025-06-25 12:47:03 +0800

    first commit

---
+++ ultralytics-main/ultralytics/nn/modules/Impove/bifpn.py
@@ -0,0 +1,41 @@
+
+
+import math
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+# 结合BiFPN 设置可学习参数 学习不同分支的权重
+# 两个分支concat操作
+class Concat_BiFPN(nn.Module):
+    def __init__(self, dimension=1):
+        super(Concat_BiFPN, self).__init__()
+        self.d = dimension
+        self.w = nn.Parameter(torch.ones(2, dtype=torch.float32), requires_grad=True)
+        self.epsilon = 0.0001
+
+    def forward(self, x):
+        w = self.w
+        weight = w / (torch.sum(w, dim=0) + self.epsilon)  # 将权重进行归一化
+        # Fast normalized fusion
+        x = [weight[0] * x[0], weight[1] * x[1]]
+        return torch.cat(x, self.d)
+
+
+# 三个分支concat操作
+class Concat_BiFPN3(nn.Module):
+    def __init__(self, dimension=1):
+        super(Concat_BiFPN3, self).__init__()
+        self.d = dimension
+        self.w = nn.Parameter(torch.ones(3, dtype=torch.float32), requires_grad=True)
+        self.epsilon = 0.0001
+
+    def forward(self, x):
+        w = self.w
+        weight = w / (torch.sum(w, dim=0) + self.epsilon)  # 将权重进行归一化
+        # Fast normalized fusion
+        x = [weight[0] * x[0], weight[1] * x[1], weight[2] * x[2]]
+        return torch.cat(x, self.d)
+
